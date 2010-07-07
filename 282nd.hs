504bc80aa5dec53683ed2a5414f00d0b8369212e
skully <skully@elte.hu>
2010-07-07 03:09:37 +0200

    282nd problem is not solved, need to optimise

--- 282nd.hs
+++ 282nd.hs
@@ -1,9 +1,12 @@
 module Main where
 
-main = print $ (sum [ack x x | x<-[1..6]]) `rem` (14^8)
-
+main = print $ (sum [ack x x | x<-[1..6]])
+--main= print $ [ack 2 x | x<-[0..10]]
 
 ack m n 
      | m==0 = n+1
+     | m==1 = n+2
+     | m==2 = 2*n+3
+     | m== 3 = 2^(n+3)-3
      | m > 0 && n == 0 = ack (m-1) 1
      | m > 0 && n > 0 = ack (m-1) (ack m (n-1))
